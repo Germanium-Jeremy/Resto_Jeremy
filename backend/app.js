6cf256b2306a77e9643e4a7ff5f37fa2a27a4cd5
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-07-03 19:15:44 +0200

    cors credintial false

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: false
 }));
 
 // ROUTES CONFIGURATIONS

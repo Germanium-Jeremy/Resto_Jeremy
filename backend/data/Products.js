3bf7fa57fcca5851cb6a50d7d3b74faac354fca8
Germanium-Jeremy <nkundabagenzijeremy@gmail.com>
2024-08-03 17:17:30 +0200

    adjustiments for error

--- backend/data/Products.js
+++ backend/data/Products.js
@@ -1,6 +1,6 @@
 const data = [
   {    id: 1, name: ["banana.png"], keywords: ["banana", "banane", "matoke", "plantation", "food", "yellow", "breakfast", "lunch", "dinner", "supper", "desset"], normalPrice: 100, discount: 0, images: [] },
-  {    id: 2, name: ["bread.png", "bread2.png"], keywords: ["bread", "sandwitch", "food", "breakfast", "morning"], normalPrice: 1000, discount: 0, images: [] },
+  {    id: 2, name: ["bread.png", "bread2.png"], keywords: ["bread", "sandwitch", "food", "breakfast", "morning"], normalPrice: 800, discount: 0, images: [] },
   {    id: 3, name: ["burger.png", "burger1.png"], keywords: ["burger", "lunch", "expensive", ""], normalPrice: 2000, discount: 3, images: [] },
   {    id: 4, name: ["caserole.png", "caserole1.png"], keywords: ["caserole", "dish", "supper", "dinner", "meal", "food"], normalPrice: 4000, discount: 10, images: [] },
   {    id: 5, name: ["cheese 1.png", "cheese 2.png", "cheese 3.png"], keywords: ["cheese", "burger", "sandwitch", "morning", "breakfast"], normalPrice: 1000, discount: 10, images: [] },
